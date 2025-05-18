1d4566d0147b7e8c13c68bd74782c47aeed2c8fe
Khine-Zin <lonelone582180@gmail.com>
2025-05-19 03:23:24 +0630

    task commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // Use relative path for assets
   plugins: [react()],
-
-  
 });
