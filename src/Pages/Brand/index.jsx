c2142c5cba5b005108320317237afdfb8c16e88c
Khine-Zin <lonelone582180@gmail.com>
2025-05-27 15:54:26 +0630

    task  commit

--- src/Pages/Brand/index.jsx
+++ src/Pages/Brand/index.jsx
@@ -72,7 +72,7 @@ const Brand = () => {
     const navigate = useNavigate();
   const isTabletOrSmaller = useMediaQuery('(max-width: 960px)');
   const [data, setData] = React.useState([]);
-  const [pager, setPager] = React.useState({ currentPage: 1, pageSize: 10 });
+  const [pager, setPager] = React.useState({ currentPage: 1, pageSize: 13 });
   const debounceTimer = React.useRef(null);
   const [bannerImage, setBannerImage] = React.useState("");
   const [brand,setBrand]=useState("")
